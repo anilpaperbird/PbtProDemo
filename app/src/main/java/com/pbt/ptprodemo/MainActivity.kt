e76c2ddfb78c1c3089dda5723373cdee8f137309
Thakor Anil <rahul.thakor@paperbirdtech.com>
2024-04-26 16:51:40 +0530

    Made changes via Node.js

--- app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
+++ app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
@@ -11,11 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        //test commite dsf sdfd
-        //new  CHANG E dasf dgsdgsgd
-
-        //i made new changes 
-        //dfgsdg
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
