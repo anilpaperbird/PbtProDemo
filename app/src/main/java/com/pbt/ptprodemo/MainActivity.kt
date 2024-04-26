862d9cfd5f296cd64589e249105479479db9934f
Thakor Anil <rahul.thakor@paperbirdtech.com>
2024-04-26 16:44:18 +0530

    Made changes via Node.js

--- app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
+++ app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         //new  CHANG E dasf dgsdgsgd
 
         //i made new changes 
+        //dfgsdg
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
