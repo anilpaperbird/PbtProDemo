10d8f10f94a68e8cd076621c5fcb1075bbdc77f4
Thakor Anil <rahul.thakor@paperbirdtech.com>
2024-04-30 11:43:09 +0530

    30-04-2024 Package Name Changed

--- app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
+++ app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.pbtpro.sarkar.R
+import com.pbtpro.ptprodemo.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
