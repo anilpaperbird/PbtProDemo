909c2fcbd80b6b5cd08792a73993124c63f087f3
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-04-30 12:34:03 +0530

    Add Run Project test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,11 +14,11 @@ android {
         }
     }
 
-    namespace = "com.pbtpro.testtwo"
+    namespace = "com.pbtpro.testone"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.pbtpro.testtwo"
+        applicationId = "com.pbtpro.testone"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
