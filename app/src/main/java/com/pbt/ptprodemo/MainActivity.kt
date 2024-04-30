a08e3bae8f4ed93abf078fecf40966411d43ca3a
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-04-30 12:42:02 +0530

    Add Run Project test

--- app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
+++ app/src/main/java/com/pbt/ptprodemo/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-    //tesst11
+    //tesst1122
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
