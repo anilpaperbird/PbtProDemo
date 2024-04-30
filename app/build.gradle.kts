baaa7a6b258887435cb70bbe48aeb6d4c4be9269
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-04-30 14:47:20 +0530

    Add Run Project test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.pbtpro.secondSarkar"
+        applicationId = "com.pbtpro.sarkar"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
