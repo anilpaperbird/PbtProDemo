0dceaa742214db6f32b117d811f4b01d54dbf873
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-05-01 11:55:19 +0530

    Add Run Project test

--- app/src/main/java/com/pbt/ptpro/MainActivity.kt
+++ app/src/main/java/com/pbt/ptpro/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pbtpro.sarkar.R
 
+//import com.pbtpro.sarkar.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
     //tesst1122
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
