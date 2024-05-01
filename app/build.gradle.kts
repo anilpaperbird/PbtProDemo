d187e4dd7eed209bc5788b5f80cebdfd8732b6b1
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-05-01 13:01:33 +0530

    30-04-2024 Package Name Changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.pbtpro.beta"
+        applicationId = "com.pbtpro.betaone"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
