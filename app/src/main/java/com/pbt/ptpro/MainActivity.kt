204ef6bc8c21fa5343864385f28c174491be6d08
Ravindra Dabhi <ravindra.dabhi@paperbirdtech.com>
2024-05-01 11:42:44 +0530

    Add Run Project test

--- app/src/main/java/com/pbt/ptpro/MainActivity.kt
+++ app/src/main/java/com/pbt/ptpro/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.pbtpro.ptprodemo.R
+import com.pbtpro.sarkar.R
 
 
 class MainActivity : AppCompatActivity() {
